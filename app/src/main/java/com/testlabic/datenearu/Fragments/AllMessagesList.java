53e3af6267b155d41ee02b6afae19e44b4490f6b
veloxIsTesting <aviharsh234>
2019-01-21 00:20:25 +0530

    messages_merged

--- app/src/main/java/com/testlabic/datenearu/Fragments/AllMessagesList.java
+++ app/src/main/java/com/testlabic/datenearu/Fragments/AllMessagesList.java
@@ -1,5 +1,7 @@
 package com.testlabic.datenearu.Fragments;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,10 +9,22 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,6 +34,8 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.jpardogo.android.googleprogressbar.library.GoogleProgressBar;
 import com.testlabic.datenearu.ChatUtils.ChatMessage;
+import com.testlabic.datenearu.ChatUtils.chatFullScreen;
+import com.testlabic.datenearu.Models.ConnectionViewHolder;
 import com.testlabic.datenearu.Models.ModelContact;
 import com.testlabic.datenearu.Models.ModelLastMessage;
 import com.testlabic.datenearu.R;
@@ -28,8 +44,11 @@ import com.testlabic.datenearu.Utils.Constants;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 import com.testlabic.datenearu.Adapters.LastMessageAdapter;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 /**
  * Created by wolfsoft4 on 21/9/18.
@@ -43,6 +62,8 @@ public class AllMessagesList extends Fragment {
     private RecyclerView recyclerview;
     private ArrayList<ModelLastMessage> list;
     private String status= null;
+    private RecyclerView cRecyclerView;
+    private FirebaseRecyclerAdapter cAdapter;
     
     @Nullable
     @Override
@@ -54,7 +75,7 @@ public class AllMessagesList extends Fragment {
        // ((LinearLayoutManager) layoutManager).setReverseLayout(true);
         recyclerview.setLayoutManager(layoutManager);
         recyclerview.setItemAnimator(new DefaultItemAnimator());
-        
+        cRecyclerView = view.findViewById(R.id.cRecylerView);
         //downloadDataAndSetAdapter();
         bar.setVisibility(View.VISIBLE);
         return view;
@@ -194,9 +215,66 @@ public class AllMessagesList extends Fragment {
         });
     }
     
+    
     @Override
     public void onResume() {
         super.onResume();
+        setUpListView();
+        if (cAdapter != null)
+            cAdapter.startListening();
         downloadDataAndSetAdapter();
+        
+    }
+    
+    //code for connections
+    
+    private void setUpListView() {
+        Log.e(TAG, "setting biew");
+        
+        final DatabaseReference ref = FirebaseDatabase.getInstance().getReference()
+                .child(Constants.Messages)
+                .child(Constants.uid).child(Constants.contacts);
+        
+        FirebaseRecyclerOptions<ModelContact> options = new FirebaseRecyclerOptions.Builder<ModelContact>()
+                .setQuery(ref, ModelContact.class)
+                .build();
+        cAdapter = new FirebaseRecyclerAdapter<ModelContact, ConnectionViewHolder>(options) {
+            @NonNull
+            @Override
+            public ConnectionViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View view = LayoutInflater.from(getContext())
+                        .inflate(R.layout.sample_contacts, null, false);
+                return new ConnectionViewHolder(view);
+            }
+    
+            @Override
+            protected void onBindViewHolder(@NonNull ConnectionViewHolder holder, int position, @NonNull ModelContact model) {
+                    holder.bindMember(model, getActivity(), getRef(position));
+            }
+            
+        };
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        cRecyclerView.setLayoutManager(layoutManager);
+        cRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        cRecyclerView.setAdapter(cAdapter);
+        
+        
+    }
+    
+   
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+       
+    }
+    
+    @Override
+    public void onStop() {
+        super.onStop();
+        
+        if (cAdapter != null)
+            cAdapter.stopListening();
     }
 }
--- app/src/main/java/com/testlabic/datenearu/Models/ConnectionViewHolder.java
+++ app/src/main/java/com/testlabic/datenearu/Models/ConnectionViewHolder.java
@@ -1,29 +1,22 @@
-package com.testlabic.datenearu.Fragments;
+package com.testlabic.datenearu.Models;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseListOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.testlabic.datenearu.ChatUtils.chatFullScreen;
-import com.testlabic.datenearu.Models.ModelContact;
 import com.testlabic.datenearu.R;
 import com.testlabic.datenearu.Utils.Constants;
 
@@ -31,95 +24,68 @@ import java.util.HashMap;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
-/**
- * Created by wolfsoft4 on 21/9/18.
- */
-
-public class Connections extends Fragment {
-    private ListView listView;
-    private FirebaseListAdapter<ModelContact> adapter;
-    
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.contacts, container, false);
-        listView = view.findViewById(R.id.listView);
-        setUpListView();
-        return view;
-        
+public class ConnectionViewHolder extends RecyclerView.ViewHolder {
+    View v;
+    public ConnectionViewHolder(@NonNull View itemView) {
+        super(itemView);
+        v = itemView;
     }
     
-    private void setUpListView() {
-
-        final DatabaseReference ref = FirebaseDatabase.getInstance().getReference()
-                .child(Constants.Messages)
-                .child(Constants.uid).child(Constants.contacts);
-
-        FirebaseListOptions<ModelContact> options = new FirebaseListOptions.Builder<ModelContact>()
-                .setQuery(ref, ModelContact.class)
-                .setLayout(R.layout.sample_contacts)
-                .build();
-        adapter = new FirebaseListAdapter<ModelContact>(options) {
+    public void bindMember(final ModelContact model, final Context context, final DatabaseReference ref)
+    {
+        TextView name = v.findViewById(R.id.name);
+        ImageView photo = v.findViewById(R.id.image);
+        TextView about = v.findViewById(R.id.about);
+        if(model.getOneLine()!=null)
+            about.setText(model.getOneLine());
+        name.setText(model.getName());
+        Glide.with(context).load(getBiggerImage(model.getImage())).into(photo);
+        if(model.getBlockStatus()!=null&&model.getBlockStatus())
+        {
+            //show connection as blocked
+            name.setTextColor(Color.GRAY);
+            about.setTextColor(Color.GRAY);
+            about.setText("Long press to unblock");
+        }
+        else
+        if(model.getBlockStatus()!=null&&!model.getBlockStatus())
+        {
+            name.setTextColor(context.getResources().getColor(R.color.read_color));
+            about.setText(model.getOneLine());
+        }
+    
+        v.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected void populateView(View v, final ModelContact model, final int position) {
-                TextView name = v.findViewById(R.id.name);
-                ImageView photo = v.findViewById(R.id.image);
-                TextView about = v.findViewById(R.id.about);
-                if(model.getOneLine()!=null)
-                    about.setText(model.getOneLine());
-                name.setText(model.getName());
-                Glide.with(getActivity()).load(getBiggerImage(model.getImage())).into(photo);
-                if(model.getBlockStatus()!=null&&model.getBlockStatus())
-                {
-                    //show connection as blocked
-                    name.setTextColor(Color.GRAY);
-                    about.setTextColor(Color.GRAY);
-                    about.setText("Long press to unblock");
-                }
-                else
-                    if(model.getBlockStatus()!=null&&!model.getBlockStatus())
-                    {
-                        name.setTextColor(getResources().getColor(R.color.read_color));
-                        about.setText(model.getOneLine());
-                    }
-                
-                v.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(getActivity(), chatFullScreen.class);
-    
-                        i.putExtra(Constants.sendToUid, model.getUid());
-    
-                        i.putExtra(Constants.sendToName, model.getName());
-                        startActivity(i);
-                    }
-                });
-                
-                v.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View v) {
-                        showDialog(model.getUid(), getRef(position), model.getBlockStatus());
-                        return true;
-                    }
-                });
-                
+            public void onClick(View v) {
+                Intent i = new Intent(context, chatFullScreen.class);
+            
+                i.putExtra(Constants.sendToUid, model.getUid());
+            
+                i.putExtra(Constants.sendToName, model.getName());
+                context.startActivity(i);
             }
-        };
-        listView.setAdapter(adapter);
-        
-        
+        });
+    
+        v.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                showDialog(model.getUid(), ref, model.getBlockStatus(), context);
+                return true;
+            }
+        });
+    
     }
     
-    private void showDialog(final String uid, final DatabaseReference blockRef, Boolean blockStatus) {
-    
-        final LayoutInflater factory = LayoutInflater.from(getContext());
+    private void showDialog(final String uid, final DatabaseReference blockRef, Boolean blockStatus, final Context context) {
+        
+        final LayoutInflater factory = LayoutInflater.from(v.getContext());
         final View connectionSetting = factory.inflate(R.layout.connection_setting, null);
-    
-        final SweetAlertDialog mainDialog = new SweetAlertDialog(getActivity(), SweetAlertDialog.NORMAL_TYPE)
+        
+        final SweetAlertDialog mainDialog = new SweetAlertDialog(context, SweetAlertDialog.NORMAL_TYPE)
                 .setTitleText("Choose")
                 .setCustomView(connectionSetting);
         mainDialog.show();
-    
+        
         Button block = connectionSetting.findViewById(R.id.block);
         Button delete = connectionSetting.findViewById(R.id.delete);
         Button report = connectionSetting.findViewById(R.id.report);
@@ -130,7 +96,7 @@ public class Connections extends Fragment {
                 @Override
                 public void onClick(View v) {
                     //unblock user
-                    new SweetAlertDialog(getActivity(), SweetAlertDialog.WARNING_TYPE)
+                    new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
                             .setTitleText("Unblock connection?")
                             .setContentText("You will be able to send/receive further messages again")
                             .setConfirmButton("Proceed?", new SweetAlertDialog.OnSweetClickListener() {
@@ -142,16 +108,16 @@ public class Connections extends Fragment {
                                             .child(Constants.blockList)
                                             .child(Constants.uid)
                                             .child(uid);
-    
+                                    
                                     reference.setValue(null).addOnSuccessListener(new OnSuccessListener<Void>() {
                                         @Override
                                         public void onSuccess(Void aVoid) {
                                             sweetAlertDialog.changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
                                             //update the contact reference
-    
+                                            
                                             HashMap<String, Object> updateBlockStat = new HashMap<>();
                                             updateBlockStat.put("blockStatus", false);
-    
+                                            
                                             blockRef.updateChildren(updateBlockStat).addOnSuccessListener(new OnSuccessListener<Void>() {
                                                 @Override
                                                 public void onSuccess(Void aVoid) {
@@ -159,7 +125,7 @@ public class Connections extends Fragment {
                                                     mainDialog.hide();
                                                 }
                                             });
-    
+                                            
                                         }
                                     });
                                 }
@@ -171,7 +137,7 @@ public class Connections extends Fragment {
             block.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    new SweetAlertDialog(getActivity(), SweetAlertDialog.WARNING_TYPE)
+                    new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
                             .setTitleText("Block connection?")
                             .setContentText("You will not be able to send/receive further messages")
                             .setConfirmButton("Proceed?", new SweetAlertDialog.OnSweetClickListener() {
@@ -183,16 +149,16 @@ public class Connections extends Fragment {
                                             .child(Constants.blockList)
                                             .child(Constants.uid)
                                             .child(uid);
-    
+                                    
                                     reference.setValue(uid).addOnSuccessListener(new OnSuccessListener<Void>() {
                                         @Override
                                         public void onSuccess(Void aVoid) {
                                             sweetAlertDialog.changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
                                             //update the contact reference
-    
+                                            
                                             HashMap<String, Object> updateBlockStat = new HashMap<>();
                                             updateBlockStat.put("blockStatus", true);
-    
+                                            
                                             blockRef.updateChildren(updateBlockStat).addOnSuccessListener(new OnSuccessListener<Void>() {
                                                 @Override
                                                 public void onSuccess(Void aVoid) {
@@ -200,7 +166,7 @@ public class Connections extends Fragment {
                                                     mainDialog.hide();
                                                 }
                                             });
-    
+                                            
                                         }
                                     });
                                 }
@@ -208,20 +174,20 @@ public class Connections extends Fragment {
                             .show();
                 }
             });
-        
+            
         }
         
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-    
-                new SweetAlertDialog(getActivity(), SweetAlertDialog.WARNING_TYPE)
+                
+                new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
                         .setTitleText("Delete Connection?")
                         .setContentText("Any message sent to you by the connection will not reach you anymore!")
                         .setConfirmButton("Yes", new SweetAlertDialog.OnSweetClickListener() {
                             @Override
                             public void onClick(final SweetAlertDialog sweetAlertDialog) {
-    
+                                
                                 DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
                                         .child(Constants.Messages)
                                         .child(Constants.uid).child(Constants.contacts)
@@ -233,7 +199,7 @@ public class Connections extends Fragment {
                                         sweetAlertDialog.dismiss();
                                     }
                                 });
-    
+                                
                             }
                         }).show();
             }
@@ -242,19 +208,19 @@ public class Connections extends Fragment {
         report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child(Constants.reportedUsers)
-                            .child(uid)
-                            .child(Constants.uid)
-                            .push();
-                    String report = uid + " was reported by "+Constants.uid;
-                    
-                    reference.setValue(report).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Toast.makeText(getActivity(), "User reported!", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                    
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child(Constants.reportedUsers)
+                        .child(uid)
+                        .child(Constants.uid)
+                        .push();
+                String report = uid + " was reported by "+Constants.uid;
+                
+                reference.setValue(report).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(context, "User reported!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                
             }
         });
         
@@ -271,23 +237,8 @@ public class Connections extends Fragment {
             /*else if (url.contains("facebook.com"))
                 modifiedImageUrl = url.replace("picture", "picture?height=500");*/
         }
-
+        
         return modifiedImageUrl;
         
-    }
-    
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (adapter != null)
-            adapter.startListening();
-    }
-    
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        if (adapter != null)
-            adapter.stopListening();
     }
 }
