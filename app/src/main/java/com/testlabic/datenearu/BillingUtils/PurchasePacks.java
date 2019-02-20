2cdc968262dc36c67219dcace45a07f2f2ad3e5b
veloxIsTesting <aviharsh234>
2019-02-20 10:07:47 +0530

    changing_update_location

--- app/src/main/java/com/testlabic/datenearu/BillingUtils/PurchasePacks.java
+++ app/src/main/java/com/testlabic/datenearu/BillingUtils/PurchasePacks.java
@@ -45,7 +45,7 @@ import static com.startapp.android.publish.adsCommon.StartAppAd.AdMode.VIDEO;
 public class PurchasePacks extends AppCompatActivity implements PurchasesUpdatedListener {
     private static final String TAG = PurchasePacks.class.getSimpleName();
     public BillingClient mBillingClient;
-    TextView pack1;
+    TextView pack1, pack2, pack3;
     StartAppAd startAppAd;
     
     @Override
@@ -53,32 +53,34 @@ public class PurchasePacks extends AppCompatActivity implements PurchasesUpdated
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_packs);
         pack1 = findViewById(R.id.firstPack);
+        pack2 = findViewById(R.id.secondPack);
+        pack3 = findViewById(R.id.thirdPack);
+        
         TextView rewardAd = findViewById(R.id.rewardAd);
         startAppAd = new StartAppAd(PurchasePacks.this);
         rewardAd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               
-               // Log.e(TAG, "Click receied to play ad!");
-               // startAppAd.loadAd(StartAppAd.AdMode.REWARDED_VIDEO);
+                
+                // Log.e(TAG, "Click receied to play ad!");
+                // startAppAd.loadAd(StartAppAd.AdMode.REWARDED_VIDEO);
                 startAppAd.loadAd(StartAppAd.AdMode.REWARDED_VIDEO, new AdEventListener() {
                     @Override
                     public void onReceiveAd(Ad ad) {
-                        Log.i("startApp","rewarded Loaded");
+                        Log.i("startApp", "rewarded Loaded");
                         startAppAd.showAd();
-            
                     }
-        
+                    
                     @Override
                     public void onFailedToReceiveAd(Ad ad) {
-            
-                        Toast.makeText(getApplicationContext(),ad.getErrorMessage(),Toast.LENGTH_LONG).show();
+                        
+                        Toast.makeText(getApplicationContext(), ad.getErrorMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
                 startAppAd.setVideoListener(new VideoListener() {
                     @Override
                     public void onVideoCompleted() {
-                        Log.i("VideoWatched","watched");
+                        Log.i("VideoWatched", "watched");
                         startAppAd.close();
                         final DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
                                 .child(Constants.xPoints)
@@ -86,20 +88,19 @@ public class PurchasePacks extends AppCompatActivity implements PurchasesUpdated
                         reference.addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-    
+                                
                                 ModelSubscr subscr = dataSnapshot.getValue(ModelSubscr.class);
-                                if(subscr!=null)
-                                {
+                                if (subscr != null) {
                                     int current = subscr.getXPoints();
                                     
-                                        current += Constants.rewardAdPoints;
-                                        HashMap<String, Object> updatePoints = new HashMap<>();
-                                        updatePoints.put(Constants.xPoints, current);
-    
+                                    current += Constants.rewardAdPoints;
+                                    HashMap<String, Object> updatePoints = new HashMap<>();
+                                    updatePoints.put(Constants.xPoints, current);
+                                    
                                     reference.updateChildren(updatePoints).addOnSuccessListener(new OnSuccessListener<Void>() {
                                         @Override
                                         public void onSuccess(Void aVoid) {
-            
+                                            
                                             Toast.makeText(PurchasePacks.this, "You received 20 points, watch again to get more!", Toast.LENGTH_SHORT).show();
                                             Handler handler = new Handler();
                                             handler.postDelayed(new Runnable() {
@@ -112,22 +113,25 @@ public class PurchasePacks extends AppCompatActivity implements PurchasesUpdated
                                     });
                                 }
                             }
-    
+                            
                             @Override
                             public void onCancelled(@NonNull DatabaseError databaseError) {
-        
+                            
                             }
                         });
                         
-                       
                     }
                 });
             }
         });
         
         // create new Person
-        //testData();
-        
+        testData();
+        pack1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
     
     private void testData() {
@@ -166,7 +170,10 @@ public class PurchasePacks extends AppCompatActivity implements PurchasesUpdated
     
     private void queryPackages() {
         List<String> skuList = new ArrayList<>();
-        skuList.add("186_points");
+        skuList.add("100_drops");
+        skuList.add("200_drops");
+        skuList.add("500_drops");
+        skuList.add("1000_drops");
         SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
         params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP);
         mBillingClient.querySkuDetailsAsync(params.build(),
@@ -177,68 +184,94 @@ public class PurchasePacks extends AppCompatActivity implements PurchasesUpdated
                         if (responseCode == BillingClient.BillingResponse.OK
                                 && skuDetailsList != null) {
                             for (final SkuDetails skuDetails : skuDetailsList) {
-                                final String desc = skuDetails.getDescription();
+                                String desc = skuDetails.getDescription();
                                 final String sku = skuDetails.getSku();
                                 String price = skuDetails.getPrice();
-                                
-                                pack1.setText(desc);
-                                
-                                pack1.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        
-                                        BillingFlowParams flowParams = BillingFlowParams.newBuilder()
-                                                .setSku("android.test.purchased")
-                                                .setType(BillingClient.SkuType.INAPP) // SkuType.SUB for subscription
-                                                .build();
-                                        int r = mBillingClient.launchBillingFlow(PurchasePacks.this, flowParams);
-    
-                                    }
+                                if (sku != null && sku.equals("100_drops")) {
+                                    desc = desc + " for " + price;
+                                    pack1.setText(desc);
+                                    pack1.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            BillingFlowParams flowParams = BillingFlowParams.newBuilder()
+                                                    .setSku(sku)
+                                                    .setType(BillingClient.SkuType.INAPP) // SkuType.SUB for subscription
+                                                    .build();
+                                            int responseCode = mBillingClient.launchBillingFlow(PurchasePacks.this, flowParams);
+                                        }
+                                    });
+                                } else if (sku != null && sku.equals("500_drops")) {
+                                    desc = desc + " for " + price;
+                                    pack2.setText(desc);
+                                    pack2.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            BillingFlowParams flowParams = BillingFlowParams.newBuilder()
+                                                    .setSku(sku)
+                                                    .setType(BillingClient.SkuType.INAPP) // SkuType.SUB for subscription
+                                                    .build();
+                                            int responseCode = mBillingClient.launchBillingFlow(PurchasePacks.this, flowParams);
+                                        }
+                                    });
+                                } else if (sku != null && sku.equals("1000_drops")) {
+                                   
+                                    desc = desc + " for "+price;
+                                    pack3.setText(desc);
+                                    pack3.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            BillingFlowParams flowParams = BillingFlowParams.newBuilder()
+                                                    .setSku(sku)
+                                                    .setType(BillingClient.SkuType.INAPP) // SkuType.SUB for subscription
+                                                    .build();
+                                            int responseCode = mBillingClient.launchBillingFlow(PurchasePacks.this, flowParams);
+                                        }
                                     });
                                 }
                             }
                         }
-                        // Process the result.
                     }
+                    // Process the result.
+                }
         );
-                }
-        
-        @Override
-        public void onPurchasesUpdated ( int responseCode, @Nullable List<Purchase> purchases){
-            if (responseCode == BillingClient.BillingResponse.OK
-                    && purchases != null) {
-                for (Purchase purchase : purchases) {
-                    handlePurchase(purchase);
-                }
-            } else if (responseCode == BillingClient.BillingResponse.USER_CANCELED) {
-                // Handle an error caused by a user cancelling the purchase flow.
-            } else {
-                // Handle any other error codes.
-            }
-        }
-        
-        private void handlePurchase (Purchase purchase){
-            String test = purchase.getOriginalJson();
-            String sku = purchase.getSku();
-            //  if(sku.equals(Constants.firstPack))
-            {
-                //update points
-                DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
-                        .child(Constants.xPoints)
-                        .child(Constants.uid);
-                HashMap<String, Object> updatePoints = new HashMap<>();
-                updatePoints.put(Constants.xPoints, 11000);
-                reference.updateChildren(updatePoints).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        new SweetAlertDialog(PurchasePacks.this)
-                                .setTitleText("Points updated!")
-                                .setContentText("Purchase Successful")
-                                .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
-                    }
-                });
-            }
-            Log.e(TAG, test);
-        }
-        
     }
+    
+    @Override
+    public void onPurchasesUpdated(int responseCode, @Nullable List<Purchase> purchases) {
+        if (responseCode == BillingClient.BillingResponse.OK
+                && purchases != null) {
+            for (Purchase purchase : purchases) {
+                handlePurchase(purchase);
+            }
+        } else if (responseCode == BillingClient.BillingResponse.USER_CANCELED) {
+            // Handle an error caused by a user cancelling the purchase flow.
+        } else {
+            // Handle any other error codes.
+        }
+    }
+    
+    private void handlePurchase(Purchase purchase) {
+        String test = purchase.getOriginalJson();
+        String sku = purchase.getSku();
+        //  if(sku.equals(Constants.firstPack))
+        {
+            //update points
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                    .child(Constants.xPoints)
+                    .child(Constants.uid);
+            HashMap<String, Object> updatePoints = new HashMap<>();
+            updatePoints.put(Constants.xPoints, 1000000);
+            reference.updateChildren(updatePoints).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    new SweetAlertDialog(PurchasePacks.this)
+                            .setTitleText("Points updated!")
+                            .setContentText("Purchase Successful")
+                            .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                }
+            });
+        }
+        Log.e(TAG, test);
+    }
+    
+}
