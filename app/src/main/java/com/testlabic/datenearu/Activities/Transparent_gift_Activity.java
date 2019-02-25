c2ca4fbed389dcac6980a7738ae8b96b984de40d
Siddharth Vaish <sidvaish97@gmail.com>
2019-02-25 19:34:13 +0530

    scratch off added

--- app/src/main/java/com/testlabic/datenearu/Activities/Transparent_gift_Activity.java
+++ app/src/main/java/com/testlabic/datenearu/Activities/Transparent_gift_Activity.java
@@ -16,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.cooltechworks.views.ScratchImageView;
+import com.jackpocket.scratchoff.ScratchoffController;
 import com.testlabic.datenearu.R;
 
 public class Transparent_gift_Activity extends Activity {
@@ -29,10 +30,53 @@ public class Transparent_gift_Activity extends Activity {
         final ImageView premium_bottle=findViewById(R.id.premium_bottle);
         ImageView regular_bottle=findViewById(R.id.royal_bottle);
         ImageView royal_bottle=findViewById(R.id.regular_bottle);
-        scratchImageView=findViewById(R.id.sample_image);
+        //scratchImageView=findViewById(R.id.sample_image);
         //scratchImageView = new ScratchImageView(this);
 
-        scratchImageView.setRevealListener(new ScratchImageView.IRevealListener() {
+
+        ScratchoffController controller = new ScratchoffController(this)
+                .setThresholdPercent(0.10d)
+                .setTouchRadiusDip(this, 30)
+                .setFadeOnClear(true)
+                .setClearOnThresholdReached(true)
+                .setCompletionCallback(new Runnable() {
+                    @Override
+                    public void run() {
+                        premium_bottle.setVisibility(View.VISIBLE);
+                        AlphaAnimation anim2 = new AlphaAnimation(0.0f, 1.0f);
+                        anim2.setStartOffset(100);
+                        anim2.setDuration(1000);
+                        //anim1.setRepeatCount(10);
+                        //anim1.setRepeatMode(Animation.ZORDER_BOTTOM);
+                        premium_bottle.startAnimation(anim2);
+                        KonfettiView viewKonfetti=findViewById(R.id.viewKonfetti);
+                        viewKonfetti.build()
+                                .addColors(getApplicationContext().getResources().getColor(R.color.appcolor),
+                                        getApplicationContext().getResources().getColor(R.color.yellow),
+                                        getApplicationContext().getResources().getColor(R.color.appcolor))
+                                .setDirection(0.0, 359.0)
+                                .setSpeed(1f, 5f)
+                                .setFadeOutEnabled(true)
+                                .setTimeToLive(2000L)
+                                .addShapes(Shape.RECT, Shape.CIRCLE)
+                                .addSizes(new Size(12, 5))
+                                .setPosition(-100f, 1000f, -50f, -50f)
+                                .streamFor(300, 5000L);
+                        Handler handler=new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                finish();
+                            }
+                        },10000);
+                    }
+                })
+                .attach(findViewById(R.id.scratch_view), findViewById(R.id.scratch_view_behind));
+
+
+
+
+        /*scratchImageView.setRevealListener(new ScratchImageView.IRevealListener() {
             @Override
             public void onRevealed(ScratchImageView tv) {
                 // on reveal
@@ -89,7 +133,7 @@ public class Transparent_gift_Activity extends Activity {
 
 
             }
-        });
+        });*/
 
     }
 }
