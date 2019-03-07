9e505b07291a292b3721e53e66f141e8b0c89710
Siddharth Vaish <sidvaish97@gmail.com>
2019-03-07 12:55:53 +0530

    serious improvement

--- app/src/main/java/com/testlabic/datenearu/Activities/SignIn.java
+++ app/src/main/java/com/testlabic/datenearu/Activities/SignIn.java
@@ -417,7 +417,7 @@ public class SignIn extends AppCompatActivity implements GoogleApiClient.OnConne
                                     imageView.setVisibility(View.INVISIBLE);
                                     //progressBar.setVisibility(View.INVISIBLE);
 
-                                    startActivity(new Intent(SignIn.this, MainActivity.class));
+                                    startActivity(new Intent(SignIn.this, NewUserSetup.class));
                                     finish();
                                     //startActivity(new Intent(SignIn.this, MainActivity.class).putExtra(Constants.refresh, true));
                                     // finish();
